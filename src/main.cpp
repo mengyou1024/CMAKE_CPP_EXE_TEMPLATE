3355a3bdf9b132e0c7b5bb4078e741a8fa9f5137
mengyou <1523038073@qq.com>
2025-01-10 10:13:18 +0800

    feat(components): 添加示例库并重构项目
    - 添加 example_lib 子模块，用于演示库的集成
    - 重构 CMakeLists.txt，引入 Utils.cmake 工具函数
    - 新增 add_subdirectory_path、resolve_source_tree 等实用函数
    - 更新 main.cpp，添加对示例库的调用

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "main.hpp"
 #include <fmt/core.h>
 
+#if __has_include(<add.hpp>)
+    #include <add.hpp>
+#else
+auto Add(auto lsh, auto rsh) {
+    return lsh + rsh;
+}
+#endif
+
 int main() {
     fmt::print(HELLO);
+    fmt::print("The sum of 3 and 4 is {}\n", Add(3, 4));
     return 0;
 }
